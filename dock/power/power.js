d356853a99559fde24c2817d6881641fc6425b31
Tanuj Sharma <107744425+Upcomingwalker@users.noreply.github.com>
2025-07-18 23:21:06 +0530

    Delete dock directory

--- dock/power/power.js
+++
@@ -1,9 +0,0 @@
-    function updateTime() {
-      const now = new Date();
-      const date = now.toLocaleDateString();
-      const time = now.toLocaleTimeString();
-      document.getElementById('clock').textContent = `${date} ${time}`;
-    }
-
-    updateTime();
-    setInterval(updateTime, 1000);
